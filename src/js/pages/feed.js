ef4d0eee2cd696e5d7f6c80a3c742fed7b151827
adrianatwatanabe <adriana.t.watanabe@gmail.com>
2022-05-08 17:29:14 -0300

    Validação de envio de novos posts

--- src/js/pages/feed.js
+++ src/js/pages/feed.js
@@ -1,5 +1,5 @@
-import { createUserPost, viewPostsCollection} from '../../../firebase-configuration/firestore.js';
-import { closeModalAutomatically} from '../components/general-site-components/modal.js';
+import { createUserPost, viewPostsCollection } from '../../../firebase-configuration/firestore.js';
+import { closeModalAutomatically } from '../components/general-site-components/modal.js';
 import { createPost } from '../components/posts/template-view-post.js';
 import { readingTextareaSize } from '../components/general-site-components/textarea-size.js';
 
@@ -32,20 +32,31 @@ export function publishPost() {
   const postContainer = document.querySelector('[data-post="container"]');
   const message = document.querySelector('#message-new-post');
   const addNewMessage = document.querySelector('#create-post');
-  const newMessage = addNewMessage.value;
+  let newMessage = addNewMessage.value;
+
+  let validatedText = newMessage.match(/[\wÀ-ú]/g); //somente caracteres
+  let validatedTextTab = newMessage.match(/[\wÀ-ú]+\n{3}/g); //caracter com uma quebra de linha
+  let validatedTabText = newMessage.match(/\n+[\wÀ-ú]/g);
+
   if (newMessage === '') {
     message.innerHTML = 'Não é possível enviar um post vazio!';
-    setTimeout(()=>{
+    setTimeout(() => {
       message.innerHTML = '';
     }, 3000);
+    addNewMessage.value = '';
+  } else if (validatedText || validatedTextTab || validatedTabText) {
+    createUserPost(newMessage).then(() => {
+      closeModalAutomatically(postClose, postContainer);
+      showAllPosts();
+      const initialSizeTextarea = document.getElementById('create-post');
+      initialSizeTextarea.setAttribute('style', 'height: 80px;');
+    });
+    addNewMessage.value = '';
   } else {
-    createUserPost(newMessage)
-      .then(()=>{
-        closeModalAutomatically(postClose, postContainer);
-        showAllPosts();
-        const initialSizeTextarea = document.getElementById('create-post');
-        initialSizeTextarea.setAttribute('style', 'height: 80px;');
-      });
+    message.innerHTML = 'Não é possível enviar um post vazio!';
+    setTimeout(() => {
+      message.innerHTML = '';
+    }, 3000);
+    addNewMessage.value = '';
   }
-  addNewMessage.value = '';
 }
