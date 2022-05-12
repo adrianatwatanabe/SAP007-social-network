b5f958490050faff6b16f2c6d3a566d606de760b
Raynara Pimenta <raynarapimenta@gmail.com>
2022-05-12 14:58:18 -0300

    testes

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,50 +1,52 @@
+/*
+ * @jest-environment jsdom
+*/
 import { registerNewUser } from '../src/config/authentication.js';
-import { loginLabFriends } from '../src/js/pages/login.js' 
+import { createRegister } from '../src/js/pages/user-register.js'; 
 
 jest.mock('../src/config/export.js');
 
-describe('Teste de tipo de função', () => {
-  it('registerNewUser é uma função', () => {
-    expect(typeof registerNewUser).toBe('function');
-  });
-  it('loginLabFriends é uma função ', () => {
-    expect(typeof loginLabFriends).toBe('function');
-  });
-});
 
-describe('registerUser', () => {
+describe('registerNewUser', () => {
   it('Deverá registrar um usuário', () => {
-    registerUser.mockResolvedValueOnce();
-      const name = 'Raynara Pimenta';
-      const email = 'raynarapimenta@gmail.com';
-      const password = '123456';
-      const page = Register();
-      const nameInput = page.querySelector('.name');
-      const emailInput = page.querySelector('.email');
-      const passwordInput = page.querySelector('.password');
-      const btnRegister = page.querySelector('#register-button');
-      nameInput.value = name;
-      emailInput.value = email;
-      passwordInput.value = password;
+    registerNewUser.mockResolvedValue();
+
+      const page = createRegister();
+      const nameInput = page.querySelector('#user-name');
+      const emailInput = page.querySelector('#user-email');
+      const passwordInput = page.querySelector('#user-password');
+      const passwordInputRepeat = page.querySelector('#user-password-repeat');
+      const btnRegister = page.querySelector('#new-login');
+      nameInput.value = 'Novo Usuario';
+      emailInput.value = 'usuario@teste.com';
+      passwordInput.value = '123456';
+      passwordInputRepeat.value = '123456';
+      const validatedEmail = emailInput.match(/[\w.\-+]+@[\w-]+\.[\w-.]+/gi);
       btnRegister.dispatchEvent(new Event('click'));
-    expect(registerUser).toHaveBeenCalledWith(name, email, password);
-    expect(registerUser).toHaveBeenCalledTimes(1);
+
+    expect(registerNewUser).toHaveBeenCalledWith(nameInput, emailInput, passwordInput);
+    expect(registerNewUser).toHaveBeenCalledTimes(1);
   });
 });
 
-describe('loginLabFriends', () => {
-  it('Deverá logar na timeline', () => {
-    signIn.mockResolvedValueOnce();
-      const email = 'raynarapimenta@gmail.com';
-      const password = '123456';
-      const page = Login();
-      const emailLogin = page.querySelector('.email-login');
-      const passwordLogin = page.querySelector('.password-login');
-      const btnLogin = page.querySelector('#signin-button');
-      emailLogin.value = email;
-      passwordLogin.value = password; 
-      btnLogin.dispatchEvent(new Event('click'));
-    expect(loginLabFriends).toHaveBeenCalledWith(email, password);
-    expect(loginLabFriends).toHaveBeenCalledTimes(1);
+
+describe('registerNewUser', () => {
+  it('Deverá registrar um usuário', () => {
+    registeNewUser.mockResolvedValue();
+    const page = createRegister();
+    const nameInput = page.querySelector('#user-name');
+    const emailInput = page.querySelector('#user-email');
+    const passwordInput = page.querySelector('#user-password');
+    const passwordInputRepeat = page.querySelector('#user-password-repeat');
+    const btnRegister = page.querySelector('#new-login');
+    nameInput.value = 'Novo usuário';
+    emailInput.value = 'usuario@teste.com';
+    passwordInput.value = '123456';
+    passwordInputRepeat.value = '123456';
+
+    const validatedEmail = emailInput.match(/[\w.\-+]+@[\w-]+\.[\w-.]+/gi);
+    btnRegister.dispatchEvent(new Event('click'));
+    expect(registerNewUser).toHaveBeenCalledWith(nameInput, emailInput, passwordInput);
+    expect(registerNewUser).toHaveBeenCalledTimes(1);
   });
 });
