aae743059aacbbf4da9b2b04cd500221c92b3092
Raynara Pimenta <raynarapimenta@gmail.com>
2022-05-17 18:28:49 -0300

    finalização dos testes

--- test/pages/user-register.spec.js
+++ test/pages/user-register.spec.js
@@ -49,7 +49,7 @@ describe('registerNewUser', () => {
     passRepeat.value = '123456';
     btnRegister.dispatchEvent(new Event('click'));
     expect(registerNewUser).not.toHaveBeenCalled();
-    expect().toEqual('Preencha o campo de email corretamente!');
+    expect(error.textContent).toEqual('Preencha o campode email corretamente!');
   });
   it('Se todos os campos estiverem vazio, deve-se mostrar o erro na tela', () => {
     registerNewUser.mockResolvedValueOnce();
@@ -59,6 +59,7 @@ describe('registerNewUser', () => {
     const password = page.querySelector('#user-password');
     const passRepeat = page.querySelector('#user-password-repeat');
     const btnRegister = page.querySelector('#new-login');
+    const error = page.querySelector('#message');
 
     name.value = '';
     email.value = '';
@@ -66,6 +67,7 @@ describe('registerNewUser', () => {
     passRepeat.value = '';
     btnRegister.dispatchEvent(new Event('click'));
     expect(registerNewUser).not.toHaveBeenCalled();
+    expect(error.textContent).toEqual('Preencha todos os campos!');
   });
   it('Se a senha e sua repetição não forem iguais, deve-se mostrar o erro na tela', () => {
     registerNewUser.mockResolvedValueOnce();
@@ -82,5 +84,23 @@ describe('registerNewUser', () => {
     passRepeat.value = '123';
     btnRegister.dispatchEvent(new Event('click'));
     expect(registerNewUser).not.toHaveBeenCalled();
+  });
+  it('Se o email já estiver cadastrado, deve-se mostrar o erro na tela', () => {
+    registerNewUser.mockResolvedValueOnce();
+    const page = createRegister();
+    const name = page.querySelector('#user-name');
+    const email = page.querySelector('#user-email');
+    const password = page.querySelector('#user-password');
+    const passRepeat = page.querySelector('#user-password-repeat');
+    const btnRegister = page.querySelector('#new-login');
+    const error = page.querySelector('#message');
+
+    name.value = 'Raynara Pimenta';
+    email.value = 'raynarapimenta@gmail.com';
+    password.value = '123456';
+    passRepeat.value = '123456';
+    btnRegister.dispatchEvent(new Event('click'));
+    expect(registerNewUser).not.toHaveBeenCalled();
+    expect(error.textContent).toEqual('Email já cadastrado! Escolha outro email.');
   });
 });
